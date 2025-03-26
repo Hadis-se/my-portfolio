d2fb77a3662ce3074bf263f4a8ab38b127c70090
Hadis <sedaghat.hadis@gmail.com>
2025-03-26 13:06:48 -0400

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./data/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
